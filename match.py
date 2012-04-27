bd0c387e72a5954242be7328eca0329ac50ce859
Peter Law <PeterJCLaw@gmail.com>
2012-04-27 20:30:01 +0100

    Move to using Match, expand the Robot.

--- match.py
+++ match.py
@@ -1,4 +1,7 @@
 
+from arena import Arena
+from robot import Robot
+
 class Match(object):
 
 	def __init__(self):
@@ -6,9 +9,10 @@ class Match(object):
 		self._game_start = Event()
 
 		robots = []
+		arena = Arena(8.0, 8.0)
 
 		for i in range(4):
-			r = Robot(i, self._game_start)
+			r = Robot(i, self, arena)
 			robots.append(r)
 			t = Thread( target = r.run, args = (i, ) )
 			t.start()
@@ -16,6 +20,17 @@ class Match(object):
 		for r in robots:
 			r.addOpponents(robots)
 
+	def waitForStart(self):
+		self._game_start.wait()
+
 	def start(self)
 		"Start the game"
 		self._game_start.set()
+
+if __name__ == '__main__':
+	m = Match()
+	m.start()
+
+	sleep(180)	# 3 minute match
+
+	exit()
--- robot.py
+++ robot.py
@@ -1,11 +1,16 @@
 
 from point import Point
 
+from datetime import timedelta, datetime
+import time
+
 class Robot(object):
-	def __init__(self, id, game_start):
+	_speed = 0.1
+
+	def __init__(self, id, match, arena):
 		self._id = id
-		self._start = game_start
-		self._opponents = set()
+		self._match = match
+		self._arena = arena
 
 	def addOpponents(self, robots):
 		for r in robots:
@@ -13,6 +18,25 @@ class Robot(object):
 				self._opponents.add(r)
 
 	def run(self, corner, args = None):
-		self._start.wait()
-		self._location = Point()
-		print 'Game started: %d' % self._id
+		self._location = self._arena.corner_location(corner)
+		self._match.waitForStart()
+#		print 'Game started: %d' % self._id
+
+		while True:
+			target = self.getTarget()
+			self.moveTowards(target)
+
+	def moveTowards(self, target):
+		"""
+		Move towards the given Point for about a second
+		"""
+
+		dist = target * self._speed
+
+		end = datetime.now() + timedelta(seconds = 1)
+		while datetime.now() < end:
+			time.sleep(0.1)
+			self.location = self.location + dist
+
+	def getTarget(self):
+		"Figure out where to move towards"
