1fbea5f88600556d313553f6c4f73e80a3bbc886
Peter Law <PeterJCLaw@gmail.com>
2012-04-28 01:21:16 +0100

    Make maths with the Point & Vector classes return suitable things.
    Also tests.

--- point.py
+++ point.py
@@ -1,10 +1,102 @@
 
-class Point(complex):
+class Vector(complex):
 	def __init__(self, x = 0, y = 0):
-		super(Point, self).__init__(x, y)
+		x = float(x)
+		y = float(y)
+		super(Vector, self).__init__(x, y)
 
 	def __repr__(self):
-		return "Point(%d, %d)" % (self.x, self.y)
+		return "Vector(%d, %d)" % (self.x, self.y)
+
+	def __add__(self, other):
+		if not isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__add__(other)
+		return Vector(res.real, res.imag)
+
+	def __sub__(self, other):
+		if not isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__sub__(other)
+		return Vector(res.real, res.imag)
+
+	def __mul__(self, other):
+		if isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__mul__(other)
+		return Vector(res.real, res.imag)
+
+	def __floordiv__(self, other):
+		if isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__floordiv__(other)
+		return Vector(res.real, res.imag)
+
+	def __mod__(self, other):
+		return NotImplemented
+
+	def __divmod__(self, other):
+		return NotImplemented
+
+	def __div__(self, other):
+		if isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__div__(other)
+		return Vector(res.real, res.imag)
+
+	def __truediv__(self, other):
+		if isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__truediv__(other)
+		return Vector(res.real, res.imag)
+
+	def __pow__(self, other):
+		return NotImplemented
+
+	def __radd__(self, other):
+		if not isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__radd__(other)
+		return Vector(res.real, res.imag)
+
+	def __rsub__(self, other):
+		if not isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__rsub__(other)
+		return Vector(res.real, res.imag)
+
+	def __rmul__(self, other):
+		if isinstance(other, complex):
+			return NotImplemented
+
+		res = super(Vector, self).__rmul__(other)
+		return Vector(res.real, res.imag)
+
+	def __rdiv__(self, other):
+		return NotImplemented
+
+	def __rtruediv__(self, other):
+		return NotImplemented
+
+	def __rfloordiv__(self, other):
+		return NotImplemented
+
+	def __rmod__(self, other):
+		return NotImplemented
+
+	def __rdivmod__(self, other):
+		return NotImplemented
+
+	def __rpow__(self, other):
+		return NotImplemented
 
 	@property
 	def x(self):
@@ -14,6 +106,21 @@ class Point(complex):
 	def y(self):
 		return self.imag
 
-class Vector(Point):
+class Point(Vector):
 	def __repr__(self):
-		return "Vector(%d, %d)" % (self.x, self.y)
+		return "Point(%d, %d)" % (self.x, self.y)
+
+	def __mul__(self, other):
+		return NotImplemented
+
+	def __floordiv__(self, other):
+		return NotImplemented
+
+	def __div__(self, other):
+		return NotImplemented
+
+	def __truediv__(self, other):
+		return NotImplemented
+
+	def __rmul__(self, other):
+		return NotImplemented
--- test.py
+++ test.py
@@ -2,9 +2,47 @@
 
 import unittest
 
-from point import Point
+from point import Point, Vector
 from arena import Arena
 import util
+
+class VectorTests(unittest.TestCase):
+
+	def setUp(self):
+		self._v = Vector(3, 4)
+		self._dbl = Vector(6, 8)
+		self._hlf = Vector(1.5, 2)
+
+	def testMod(self):
+		modv = abs(self._v)
+		util.assertEqual(5, modv)
+
+	def testRMultiply(self):
+		v = self._v * 2
+		util.assertEqual(self._dbl, v)
+
+	def testMultiply(self):
+		v = 2 * self._v
+		util.assertEqual(self._dbl, v)
+
+	def testRDiv(self):
+		v = self._v / 2
+		util.assertEqual(self._hlf, v)
+
+	def testDiv(self):
+		def go():
+			2 / self._v
+		self.assertRaises(TypeError, go)
+
+	def testVMultiply(self):
+		def go():
+			self._hlf * self._v
+		self.assertRaises(TypeError, go)
+
+	def testVDiv(self):
+		def go():
+			self._hlf / self._v
+		self.assertRaises(TypeError, go)
 
 class PointTests(unittest.TestCase):
 
