e5d22afb8637a93e063e061c324b5d44ce037153
Peter Law <PeterJCLaw@gmail.com>
2012-04-28 02:00:51 +0100

    Make the match output status data once started.

--- match.py
+++ match.py
@@ -1,4 +1,6 @@
 
+import json
+import time
 from threading import Thread, Event
 
 from arena import Arena
@@ -10,17 +12,17 @@ class Match(object):
 		# Game start lock
 		self._game_start = Event()
 
-		robots = []
+		self._robots = []
 		arena = Arena(8.0, 8.0)
 
 		for i in range(4):
 			r = Robot(i, self, arena)
-			robots.append(r)
+			self._robots.append(r)
 			t = Thread( target = r.run, args = (i, ) )
 			t.start()
 
-		for r in robots:
-			r.addOpponents(robots)
+		for r in self._robots:
+			r.addOpponents(self._robots)
 
 	def waitForStart(self):
 		assert self._game_start.wait(3)
@@ -29,10 +31,13 @@ class Match(object):
 		"Start the game"
 		self._game_start.set()
 
+		while True:
+			for r in self._robots:
+				pos = r.location
+				pos = dict(x = pos.x, y = pos.y)
+				print json.dumps(dict(id = r.id, pos = pos))
+			time.sleep(1)
+
 if __name__ == '__main__':
 	m = Match()
 	m.start()
-
-	sleep(180)	# 3 minute match
-
-	exit()
