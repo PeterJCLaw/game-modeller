eab6520e0c511123202c305dee80c103c8c8f948
Peter Law <PeterJCLaw@gmail.com>
2012-04-29 01:10:58 +0100

    Add the start of a state viewer.

---
+++ viewer/glutils.py
@@ -0,0 +1,35 @@
+
+import pyglet.gl
+
+def quad_vertices(origin, size):
+	idx = [0, 1, 2, 0, 2, 3]
+	verts = ( origin[0]          , origin[1],
+	          origin[0] + size[0], origin[1],
+	          origin[0] + size[0], origin[1] + size[1],
+	          origin[0]          , origin[1] + size[1] )
+
+	return ( idx, verts )
+
+def sqaure_vertices(origin, size):
+	return quad_vertices(origin, (size, size))
+
+def quad_colours(colour):
+	colours = ( colour[0], colour[1], colour[2],
+	            colour[0], colour[1], colour[2],
+	            colour[0], colour[1], colour[2],
+	            colour[0], colour[1], colour[2] )
+
+	return colours
+
+def batch_quad(batch, origin, size, colour = (255, 255, 255)):
+	(idx, verts) = quad_vertices(origin, size)
+	colours = quad_colours(colour)
+	quad = batch.add_indexed(4, pyglet.gl.GL_TRIANGLES, None,
+	                         idx, ('v2f', verts ),
+	                         ('c3B', colours )
+	                        )
+
+	return quad
+
+def batch_sqaure(batch, origin, size, colour):
+	return batch_quad(batch, origin, (size, size), colour)
---
+++ viewer/random-walk.py
@@ -0,0 +1,37 @@
+
+import pyglet
+from pyglet.graphics import Batch
+from random import randint
+
+from glutils import *
+
+window = pyglet.window.Window(width=800, height=800)
+batch = Batch()
+
+x = y = 100
+my_square = batch_sqaure(batch, (x, y), 20, (127, 0, 255))
+
+@window.event
+def on_draw():
+#	print 'Draw'
+	window.clear()
+	batch.draw()
+
+def limit(n, bottom, top):
+	return min(top, max(n, bottom))
+
+def update(dt):
+	global x, y
+	mov = 10
+	x_mod = randint(-1 * mov, mov)
+	y_mod = randint(-1 * mov, mov)
+
+	x = limit(x + x_mod, 0, 780)
+	y = limit(y + y_mod, 0, 780)
+
+	(idx, verts) = sqaure_vertices((x,y), 20)
+	my_square.vertices = verts
+
+pyglet.clock.schedule_interval(update, 0.01)
+
+pyglet.app.run()
